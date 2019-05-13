b3b1b40b212be44f47ac48e8afa3c4cd605cf17a
lae0901 <stephenrichter@gmail.com>
2019-05-13 03:20:27 -0400

    strip to basics

--- media/main.js
+++ media/main.js
@@ -1,113 +1,15 @@
-let global_vscode = null ;
-
-// This script will be run within the webview itself
-// It cannot access the main VS Code APIs directly.
-(function () {
-    global_vscode = acquireVsCodeApi();
-    const vscode = global_vscode ;
-
-    const oldState = vscode.getState();
-
-    const counter = document.getElementById('lines-of-code-counter');
-    console.log(oldState);
-    let currentCount = (oldState && oldState.count) || 0;
-    counter.textContent = currentCount;
-
-    if ( 1 == 2 )
-    {
-    setInterval(() => {
-        counter.textContent = currentCount++;
-
-        // Update state
-        vscode.setState({ count: currentCount });
-
-        // Alert the extension when the cat introduces a bug
-        if (Math.random() < Math.min(0.001 * currentCount, 0.05)) {
-            // Send a message back to the extension
-            vscode.postMessage({
-                command: 'alert',
-                text: '🐛  on line ' + currentCount
-            });
-        }
-    }, 100);
-    }
-
-    // Handle messages sent from the extension to the webview
-    window.addEventListener('message', event => {
-        const message = event.data; // The json data that the extension sent
-        switch (message.command) {
-            case 'refactor':
-                currentCount = Math.ceil(currentCount * 0.5);
-                counter.textContent = currentCount;
-                break;
-        }
-    });
-
-    const but1_elem = document.getElementById('but1');
-    but1_elem.addEventListener('click', ok_click);
-
+(function ()
+{
+	const but1_elem = document.getElementById('but1');
+	but1_elem.addEventListener('click', ok_click);
 }());
 
+// click a button.  show some text in the webview.
 async function ok_click()
 {
-	const elem = document.getElementById('but1');
-	let text = elem.innerText;
-	if (text == 'ok')
-			elem.innerText = 'cancel';
-	else
-			elem.innerText = 'ok';
+	const elem = document.getElementById('panel1');
+    let text = elem.innerText;
+    text += 'more text' ;
+    elem.innerText = text ;
 	text = elem.innerText;
-
-	const vscode = global_vscode ;
-	vscode.setState({ text });
-
-	const objName = 'Q*';
-	const libName = 'SR';
-	const object_list = await as400_srcfList(objName, libName);
-	text = 'number objects:' + object_list.length ;
-
-	// Send a message back to the extension
-	vscode.postMessage({
-		command: 'alert',
-		text: text
-	});
-}
-
-// ------------------------- object_toQueryString ---------------------------------
-function object_toQueryString(obj)
-{
-    const qs = Object.keys(obj)
-        .map((key) => encodeURIComponent(key) + '=' + encodeURIComponent(obj[key]))
-        .join('&');
-    return qs;
-}
-
-// --------------------- as400_srcfList_fetch -----------------------
-function as400_srcfList_fetch(objName, libName)
-{
-	const promise = new Promise(async (resolve, reject) =>
-	{
-    const libl = 'couri7 aplusb1fcc qtemp';
-    const url = 'http://173.54.20.170:10080/coder/common/json_getManyRows.php';
-		const params =
-		{
-			libl, proc: 'utl8020_srcfList',
-			parm1: objName, parm2: libName, debug: 'N'
-    };
-    const query = object_toQueryString(params);
-    const url_query = url + '?' + query ;
-
-		const response = await fetch(url_query,
-		{
-			method: 'GET',
-			headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-		});
-
-    const respText = await response.text();
-    const rows = JSON.parse(respText);
-
-    resolve(rows);
-	});
-
-	return promise;
 }
--- src/extension.ts
+++ src/extension.ts
@@ -5,12 +5,6 @@ let global_webViewPanel: vscode.WebviewPanel;
 let global_disposables: vscode.Disposable[] = [];
 const global_viewType = 'wrkobjWebview';
 
-const cats = {
-	'Coding Cat': 'https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif',
-	'Compiling Cat': 'https://media.giphy.com/media/mlvseq9yvZhba/giphy.gif',
-	'Testing Cat': 'https://media.giphy.com/media/3oriO0OEd9QIDdllqo/giphy.gif'
-};
-
 export function activate(context: vscode.ExtensionContext)
 {
 	const disposable = vscode.commands.registerCommand('rock.webview', () =>
@@ -45,20 +39,14 @@ function scriptUri_builder(
 	return scriptUri ;
 }
 
-function global_getWebviewContent(catGif: string, extPath: string)
+function global_getWebviewContent(extPath: string)
 {
 	const main_uri = scriptUri_builder( extPath, 'media', 'main.js') ;
-	const common_uri = scriptUri_builder( extPath, 'media', 'common.js') ;
 	const app_uri = scriptUri_builder( extPath, 'media', 'app.js') ;
 	const vue_uri = scriptUri_builder( extPath, 'media', 'vue.min.js') ;
 
 	// Use a nonce to whitelist which scripts can be run
 	const nonce = global_getNonce();
-
-//	<script  nonce="${nonce}" src = "https://code.jquery.com/jquery-3.3.1.min.js" > </script>
-	// <script nonce="${nonce} src="https://cdnjs.cloudflare.com/ajax/libs/vue/2.5.21/vue.min.js">
-	// </script>
-
 
 	return `<!DOCTYPE html>
 		<html lang="en">
@@ -70,10 +58,10 @@ function global_getWebviewContent(catGif: string, extPath: string)
 			<title>wrkobj webview</title>
 		</head>
 		<body>
-			<img src="${catGif}" width="300" />
-			<h1 id="lines-of-code-counter">0</h1>
 
 			<button id="but1">button</button>
+			<div id="panel1"></div>
+			<h1 id="lines-of-code-counter">0</h1>
 
 			<div id="app">
 			{{message}}
@@ -81,7 +69,6 @@ function global_getWebviewContent(catGif: string, extPath: string)
 			</div>
 
 			<script nonce="${nonce}" src="${vue_uri}"></script>
-			<script nonce="${nonce}" src="${common_uri}"></script>
 			<script nonce="${nonce}" src="${main_uri}"></script>
 			<script nonce="${nonce}" src="${app_uri}"></script>
 
@@ -127,8 +114,7 @@ function global_createPanel(extensionPath: string) {
 
 	// Set the webview's initial html content
 	const catName = 'Testing Cat';
-	const catGif = cats[catName];
-	const htmlText = global_getWebviewContent(catGif, extensionPath);
+	const htmlText = global_getWebviewContent(extensionPath);
 	global_webViewPanel.title = catName;
 	global_webViewPanel.webview.html = htmlText;
 
